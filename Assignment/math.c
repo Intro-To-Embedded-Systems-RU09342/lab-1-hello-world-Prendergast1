124858b3131fe493d39591fb4125022f96ab01cc
Prendergast1 <47788939+Prendergast1@users.noreply.github.com>
2019-10-12 12:17:19 -0400

    Add files via upload

--- Assignment/math.c
+++ Assignment/math.c
@@ -1,33 +1,71 @@
-#include <stdio.h>
+/*
+ * math.c
+ *
+ * Author: Sean Prendergast
+ */
 
-int main()
+#ifndef MATH_C_
+#define MATH_C_
+
+/* In this program, a math function was implemented
+ * to showcase its capability to perform various operations
+ * including add, subtract, multiply, integer divide and modulus.
+ * The function could also switch between various logical operations,
+ * including bitwise AND, OR, XOR (exclusive OR) and NOT (inverse)
+ * as well as shifting bits left and right.
+ */
+
+// math function to correspond with header file (math.h)
+int math(int num1, int num2, char Operator)
 {
-    int num1, num2, sum, difference, product, quotient, remainder, AND, OR, XOR, NOT;
 
-    printf("Enter two numbers \n");
-    scanf("%d%d", &num1, &num2);
+    /* switch case statements are implemented
+     * in place of several if statements to condense the number of
+     * lines of code.
+     * The switch statement allows the user to choose between different operations
+     * for the two values num1 and num2.
+     */
 
-    sum = num1 + num2;
-    difference = num1 - num2;
-    product = num1 * num2;
-    quotient = num1 / num2;
-    remainder = num1 % num2;
-    AND = num1 & num2;
-    OR = num1 | num2;
-    XOR = num1 ^ num2;
-    NOT = ~num1;
+    switch(Operator)
+    {
+        // Case statement for performing addition
+        case '+':
+            return num1 + num2;
+        // Case statement for performing subtraction
+        case '-':
+            return num1 - num2;
+        // Case statement for performing multiplication
+        case '*':
+            return num1 * num2;
+        // Case statement for performing division
+        case '/':
+            return num1 / num2;
+        // Case statement for displaying remainder
+        case '%':
+            return num1 % num2;
+        // Case statement for performing shift left
+        case '<<':
+            return num1 << num2;
+        // Case statement for performing shift right
+        case '>>':
+            return num1 >> num2;
+        // Case statement for performing bitwise AND
+        case '&':
+            return num1 & num2;
+        // Case statement for performing bitwise OR
+        case '|':
+            return num1 | num2;
+        // Case statement for performing bitwise XOR
+        case '^':
+            return num1 ^ num2;
+        // Case statement for performing bitwise NOT
+        case '~':
+            return ~num1;
+        // In the result of an error, the default argument will simply return the value of num1
+        default:
+            return num1;
+    }
 
-    printf("Sum of the numbers = %d\n", sum);
-    printf("Difference of the numbers = %d\n", difference);
-    printf("Product of the number = %d\n", product);
-    printf("Product of the number = %d\n", quotient);
-    printf("Remainder = %d\n", remainder);
-    printf("num1 << num2 = %d\n", num1 << num2);  // shift left
-    printf("num1 >> num2 = %d\n", num1 << num2);  // shift right
-    printf("AND value = %d\n",AND);
-    printf("OR value = %d\n",OR);
-    printf("XOR value = %d\n",XOR);
-    printf("NOT value = %d\n",NOT);
-
-    return 0;
 }
+
+#endif /* MATH_C_ */
